314cdd0b23f5557280ec9e58f3b5fb3a83f7f8eb
ViktorGranlof <viktor.granlof@falun.ntig.se>
2017-10-20 09:49:34 +0200

    Tal-pyramid Viktor Granlöf TE16

---
+++ Simpel Kalkylator/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkylator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simpel Kalkylator");
+            Console.WriteLine("Välj vilket räknesätt du vill använda");
+            bool resultat;
+            do
+            {
+                resultat = StartaKalkylator();      //StartaKalkylator returnerar bool in i resultat
+            }
+            while (resultat == false);
+        }
+        static bool StartaKalkylator()  //Använder bool för att tala om om programmet lyckades med beräkningen eller inte
+        {                               //Retunerar det false startas det om igen
+            Console.WriteLine("Räknesätt:");              //Skapar en meny med olika räknesätt att välja bland
+            Console.WriteLine("1: Addition");             
+            Console.WriteLine("2: Subtraktion");
+            Console.WriteLine("3: Multiplikation");
+            Console.Write("Ditt val: ");
+            String raknesatt = Console.ReadLine();
+            switch (raknesatt)
+            {
+                case "1":
+
+                    break;
+
+                case "2":         //Koden känner efter vilket tal du skrivit in
+
+                    break;
+
+                case "3":
+
+                    break;
+
+                default:
+                    Console.WriteLine("Fel inmatning!");
+                    Console.WriteLine(""); //Tom rad för att enklare utläsa kalkylatorn
+                    return false;
+            }
+            Console.Write("Tal 1: ");
+            String tal1 = Console.ReadLine();
+
+            Console.Write("Tal 2: ");
+            String tal2 = Console.ReadLine();
+
+            int tal1int = 0; //
+            int tal2int = 0;
+            try
+            {
+                tal1int = int.Parse(tal1); 
+                tal2int = int.Parse(tal2);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Fel inmatning!");
+                Console.WriteLine(""); //Tom rad för att enklare utläsa kalkylatorn
+            }
+
+            int resultatet = 0; 
+            switch (raknesatt)
+            {
+                case "1":
+                    resultatet = tal1int + tal2int; //Summan av tal1 och tal2 läggs i resultat och skriver över annan summa som legat där tidigare
+                    Console.WriteLine("Summan av " + tal1int.ToString() + " plus " + tal2int.ToString() + " är: " + resultatet.ToString());                                                                                                                       
+                    break;  //ToString betyder att man gör om talet till en string
+
+                case "2":
+                    resultatet = tal1int - tal2int;
+                    Console.WriteLine("Summan av " + tal1int.ToString() + " minus " + tal2int.ToString() + " är: " + resultatet.ToString());
+                    break;
+
+                case "3":
+                    resultatet = tal1int * tal2int;
+                    Console.WriteLine("Summan av " + tal1int.ToString() + " gånger " + tal2int.ToString() + " är: " + resultatet.ToString());
+                    break;
+            }
+            Console.ReadLine(); 
+            return true;
+        }
+    }
+}
---
+++ Tal-pyramid/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tal_pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool resultat;
+            do                          
+            {
+                resultat = TalPyramid();
+            }
+            while (resultat == false); //Kör programmet medan resultatet är falskt
+        }
+        static bool TalPyramid()
+        {
+            Console.WriteLine("Skriv in ett tal: ");
+            string input = Console.ReadLine();
+            long tal1 = 0;
+            try //Försöker köra programmet  
+            {
+                tal1 = Convert.ToInt64(input);
+            }
+            catch(Exception e) //Returnerar false om ett fel hittas som gör att programmet körs om igen
+            {
+                Console.WriteLine();
+                Console.WriteLine("Fel inmatning");
+                return false;
+            }
+            for (int i = 0; i < tal1 + 1; i++) //Kör igenom och plussar på i för varje loop
+            {
+                for (int j = 0; j < i; j++)    //Nästlad for-loop som plussar på j för varje loop
+                {
+                    Console.Write(tal1);       //Skriver ut ett visst antal tal beroende på hur många gånger loopen kördes
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Skriv något för att lämna programmet"); //Avslut
+            Console.ReadLine();
+            return true; //Returnernar true vilket gör att 'bool resultat' inte kör igen
+        }
+    }
+}
+
