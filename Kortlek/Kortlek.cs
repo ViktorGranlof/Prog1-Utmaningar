4e296eb7b1d0de9452bb71adaab5066d288a33a8
ViktorGranlof <viktor.granlof@falun.ntig.se>
2018-05-18 10:21:50 +0200

    Kommenterade kod

--- Kortlek/Kortlek.cs
+++ Kortlek/Kortlek.cs
@@ -8,25 +8,25 @@ namespace Kortlek
 {
     class Kortlek
     {
-        Random rng = new Random();
-        string[] kort = new string[52];
+        Random rng = new Random();  //Använder systemets egna random funktion
+        string[] kort = new string[52];   //Gör en array med 52 platser.
 
         public Kortlek()
         {
-            string[] cardColour = new string[4]; 
+            string[] cardColour = new string[4]; //Gör en array med kortens olika färger
             cardColour[0] = "Klöver";
             cardColour[1] = "Spader";
             cardColour[2] = "Ruter";
             cardColour[3] = "Hjärter";
 
             string[] cardValue = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Knekt", "Dam", "Kung" };
-
+                                             //En till array fast denna gång med kortens olika värden 
             int cardIndex = 0;
 
-            for (int colour = 0; colour < cardColour.Length; colour++)
+            for (int colour = 0; colour < cardColour.Length; colour++)  //Väljer en slumpmässig färg
             {
-                 for (int value = 0; value < cardValue.Length; value++)
-                 {
+                 for (int value = 0; value < cardValue.Length; value++) //Väljer ett slumpmässigt nummer
+                {
                     kort[cardIndex] = cardColour[colour] + " " + cardValue[value];
                     cardIndex++;
                  }
--- Kortlek/Program.cs
+++ Kortlek/Program.cs
@@ -11,13 +11,13 @@ namespace Kortlek
         static void Main(string[] args)
         {
             Console.Write("Hur många kort vill du dra...? ");
-            string userInput = Console.ReadLine();
-            int inputs = Convert.ToInt32(userInput);
-            Kortlek kortlek1 = new Kortlek();
+            string userInput = Console.ReadLine();  //Tar in det användaren skriver
+            int inputs = Convert.ToInt32(userInput); //Konverterar användarens input till int
+            Kortlek kortlek1 = new Kortlek();  //Anropar Kortlek
 
-            for (int i = 0; i < inputs; i++)
+            for (int i = 0; i < inputs; i++)  //När "i" är mindre än användarens input körs detta och inkrementera "i" med 1
             {
-                Console.WriteLine(kortlek1.drawCard());
+                Console.WriteLine(kortlek1.drawCard()); //Skriver ut ett random kort med hjälp av metoden drawCard i klassen Kortlek
             }
             Console.ReadLine();
         }
